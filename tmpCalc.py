ed9dc09dc738e5882f39166dc1f970fc72bca358
samarga <sanjana.s.margale@mercedes-benz.com>
2024-03-22 13:23:12 +0530

    Added the Optimized Script

--- tmpCalc.py
+++ tmpCalc.py
@@ -1,30 +1,31 @@
 # Basic Calculations
-# Calculator Program in Python by using input() and format() functions
- 
-#Promting input from the user
- 
+"""
+Calculator Program in Python by using print and arithmetic operations.
+This program calculates addition, cube, subtraction, multiplication, and division.
+"""
 
-#addition
- 
-print("{} + {} = ".format(n1, n2))
-print(n - n2)
+# Add two numbers and print the result
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+print("{} + {} = {}".format(n1, n2, n1 + n2))
 
+# Find and print the cube of a given number
+a = 2
+print("The cube of {} is {}".format(a, a**3))
 
- 
-#subtraction
- 
-print("{} - {} = ".format(n1, n2))
-print(n1 - n2 -n3)
- 
-#multiplication
- 
-print("{} * {} = ".format(n1, n2))
-print(n1 * n2)
- 
-#division
- 
-print("{} / {} = ".format(n1, n2))
-print(n1 / n2)
+# Subtract three numbers and print the result
+n3 = int(input("Enter the third number: "))
+print("{} - {} - {} = {}".format(n1, n2, n3, n1 - n2 - n3))
 
+# Multiply two numbers and print the result
+print("{} * {} = {}".format(n1, n2, n1 * n2))
+
+# Divide two numbers and print the result
+print("{} / {} = {}".format(n1, n2, n1 / n2))
+
+# Temporary function just for demonstration (Potentially to be removed)
 def tmpFunc():
-  print("Check!!!!") 
+    print("Check!!!!") 
+
+# Note: This code assumes that n1, n2, and n3 are integers and n2 is not zero for division. 
+# It's generally a good practice to include error handling for division by zero and invalid inputs.
