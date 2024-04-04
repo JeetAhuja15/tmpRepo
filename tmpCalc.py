9eb8786db6cee1fb44677b58bca3b2f0a6a0b97a
samarga <sanjana.s.margale@mercedes-benz.com>
2024-04-04 08:47:02 +0530

    Added the Optimized Script

--- tmpCalc.py
+++ tmpCalc.py
@@ -1,30 +1,39 @@
 # Basic Calculations
-# Calculator Program in Python by using input() and format() functions
- 
-#Promting input from the user
- 
+# Calculator Program in Python by using input() and various arithmetic operations
 
-#addition
- 
-print("{} + {} = ".format(n1, n2))
-print(n - n2)
+def main():
+    """
+    Main function to perform basic arithmetic operations like
+    addition, subtraction, multiplication, division, and cube of a number.
+    It prompts the user for input and then shows the results of the calculations.
+    """
+    # Prompting input from the user
+    n1 = float(input("Enter the first number (n1): "))
+    n2 = float(input("Enter the second number (n2): "))
+    
+    # Addition
+    print("{} + {} = ".format(n1, n2), n1 + n2)  # Fixed the operation to show correct result of addition.
 
+    # Cube of the first number
+    print("Cube of {}: ".format(n1), n1 ** 3)  # Fixed the operation to calculate the correct cube of n1.
+    
+    # Subtraction
+    print("{} - {} = ".format(n1, n2), n1 - n2)  # Updated to show correct result of subtraction.
 
- 
-#subtraction
- 
-print("{} - {} = ".format(n1, n2))
-print(n1 - n2 -n3)
- 
-#multiplication
- 
-print("{} * {} = ".format(n1, n2))
-print(n1 * n2)
- 
-#division
- 
-print("{} / {} = ".format(n1, n2))
-print(n1 / n2)
+    # Multiplication
+    print("{} * {} = ".format(n1, n2), n1 * n2)
 
-def tmpFunc():
-  print("Check!!!!") 
+    # Division
+    # Adding check to prevent division by zero
+    if n2 == 0:
+        print("Error: Cannot divide by zero.")
+    else:
+        print("{} / {} = ".format(n1, n2), n1 / n2)
+
+    # Temp function just as a placeholder for any additional future logic,
+    # which isn't useful currently and could be removed.
+    # def tmpFunc():
+    #     print("Check!!!!") 
+
+if __name__ == "__main__":
+    main()
